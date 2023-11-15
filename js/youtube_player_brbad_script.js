6ecdc3a1e1cabb6650ff462e5d9a93827687f86a
Sergey <simon.serezha1@yandex.ru>
2023-11-16 02:05:00 +0500

    Added youtube players for trailers

---
+++ js/youtube_player_brbad_script.js
@@ -0,0 +1,31 @@
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    width: '100%',
+    height: '500',
+    videoId: 'L5l3vv9n7EU', 
+    playerVars: {
+      autoplay: 0, 
+      rel: 0, 
+    },
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange
+    }
+  });
+}
+
+// Когда плеер готов
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// Когда состояние плеера изменяется
+function onPlayerStateChange(event) {
+}
